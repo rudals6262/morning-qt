cf40d128d96453e69083ef08ebb368bf561ffd43
rudals6262 <rudals6262@gmail.com>
2024-08-13 14:13:33 +0900

    Add Netlify deployment configurations and update API and frontend scripts

--- functions/api.js
+++ functions/api.js
@@ -2,7 +2,7 @@ const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 
 // 데이터베이스 연결
-const dbPath = path.join(__dirname, '..', '..', 'database', 'bible.db');
+const dbPath = path.join(__dirname, 'bible.db');  // 현재 디렉토리의 bible.db 파일 참조
 const db = new sqlite3.Database(dbPath, sqlite3.OPEN_READWRITE, (err) => {
     if (err) {
         console.error("Error opening database:", err.message);
